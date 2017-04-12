5b5817a539a050986b9efab3bdafc1811abcddbf
aaronvil <aaronvil@hawaii.edu>
2017-04-12 09:40:02 -1000

    made list directory

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,10 +1,7 @@
 import './home-page.html';
 
-import './host-game.html';
-
-import './club-list.html';
+import './list/club-list.html';
 
 import './faq-page.html';
 
-import './list-stuff-page.html';
-import './list-stuff-page.js';
+import './add-club-page.html';
