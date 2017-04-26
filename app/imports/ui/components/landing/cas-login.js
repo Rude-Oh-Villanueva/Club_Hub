73c994970bf3bba1c06859c263347967ceff71d8
chrisoh1 <oh2@hawaii.edu>
2017-04-25 17:19:20 -1000

    cas-login

--- app/imports/ui/components/landing/cas-login.js
+++ app/imports/ui/components/landing/cas-login.js
@@ -1,8 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 
-/* eslint-disable no-console */
-
 Template.Cas_Login.events({
   /**
    * Handle the click on the logout link.
@@ -20,14 +18,22 @@ Template.Cas_Login.events({
    * @param event The click event.
    * @returns {boolean} False.
    */
-  'click .cas-login': function casLogin(event) {
+  'click .cas-login': function casLogin(event, instance) {
     event.preventDefault();
     const callback = function loginCallback(error) {
       if (error) {
         console.log(error);
       }
     };
+    console.log("this is the callback: "+callback);
     Meteor.loginWithCas(callback);
     return false;
   },
+});
+
+// Here's how to do the required initialization for Semantic UI dropdown menus.
+Template.Cas_Login.onRendered(function enableDropDown() {
+  this.$('.dropdown').dropdown({
+    action: 'select',
+  });
 });
